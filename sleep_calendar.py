39dea694d4f6b05812d913a375a7a7caa8a0fb60
Vebjo Sletta <vebjornsletta@gmail.com>
2021-05-16 19:11:20 +0200

    Added WIP calendar plotting

--- sleep_calendar.py
+++ sleep_calendar.py
@@ -3,7 +3,21 @@ import calendar
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+import pandas as pd
 import csv
+
+def convert_time(time):
+    time = time.split(' ')
+    hm = time[0].split(':')
+    h = int(hm[0])
+    m = int(hm[1])
+    if time[1] == "pm" and h < 12:
+        h += 12 
+    if time[1] == "am" and h == 12:
+        h -= 12
+
+    return h + (m/60)
+
 
 #Gets the data from csv-file
 def get_data():
@@ -14,25 +28,43 @@ def get_data():
         #The month currently being used (only works if all data is from the same month)
         month = data[3][0][3:5]
 
+        day, start, end = [], [], []
+
         #List of sleep including date,start,end
-        sleep_data = []
         for row in data[2:]: 
             if not row:
                 continue
-            date = row[0][:2]
-            start = row[0][11:]
-            end = row[1][11:]
-            sleep_data.append([date, start, end])
+            day.append(int(row[0][:2]))
+            start.append(convert_time(row[0][11:]))
+            end.append(convert_time(row[1][11:]))
 
     file.close()
-    return sleep_data
-    
+    return day, start, end
 
 
-def create_calendar(sleep_data):
-    #plt.figure(figsize=(10,10))
-    #ax = plt.gca().axes
-    #plt.show
+#Plots the calendar using data from method above
+def create_calendar(day, start, end):
+    plt.figure(figsize=(12,7))
+    ax = plt.gca().axes
 
-sleep_data = get_data()
-create_calendar(sleep_data)
+    #adds labels
+    plt.yticks(np.arange(1, 32))
+    plt.xticks(np.arange(1,25))
+    plt.ylim(32, 1)
+    plt.xlim(1,25)
+
+    data = pd.DataFrame({"start": start, "end": end})
+
+    for s, e in zip(data['start'].values, data['end'].values):
+        ax.add_patch(Rectangle((s, day.pop(0)), width=(e-s), height=.3, color='cyan'))
+
+
+    #removes ticks and borders
+    plt.tick_params(left=False, bottom=False)
+    [spine.set_visible(False) for spine in plt.gca().spines.values()]
+
+    #show figure
+    plt.show()
+
+day, start, end = get_data()
+create_calendar(day, start, end)
